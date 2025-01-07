a96c6f9b2abcf90f4e67f2813df7631a0702c8c6
23ece220saikrishna <727823tuec220@skct.edu.in>
2025-01-07 12:22:02 +0530

    2nd commit

--- Javascript/hello.js
+++ Javascript/hello.js
@@ -1,3 +1,10 @@
-function alertmessage(){
-        alert("your form has been submitted")
-}
+echo "Enter a number: "
+read num
+original=$num
+reversed=0
+while [ $original -gt 0 ]; do
+digit=$(( $original % 10 ))
+reversed=$(( $reversed * 10 + $digit ))
+original=$(( $original / 10 ))
+done
+echo "Reversed number: $reversed"
